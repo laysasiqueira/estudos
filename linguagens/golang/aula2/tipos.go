bfd7e6dc1a95ceea7f8c3a186785cd589d7682a2
Laysa Siqueira <laysaasiqueiraa@gmail.com>
2024-05-15 10:27:24 +0100

    aula2

---
+++ linguagens/golang/aula2/tipos.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//%T significa o tipo da variavel
+	//%v significa o valor armazenado
+
+	// boolean (true/false)
+	fmt.Printf("type: %T -value: %v\n", true, true)
+
+	// str - sequência de bytes
+	fmt.Printf("type: %T -value: %v\n", "laysa", "laysa")
+	fmt.Printf("type: %T -value: %v\n", "1", "1")
+
+	// int
+	fmt.Printf("type: %T -value: %v\n", 1, 1)
+
+	// float (decimal)
+	fmt.Printf("type: %T -value: %v\n", 1.233, 1.233)
+
+}
