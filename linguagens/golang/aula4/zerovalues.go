fc8cf2c37029ada43a19046be96f6ec0d9a78c02
Laysa Siqueira <laysaasiqueiraa@gmail.com>
2024-05-19 19:18:14 +0100

    declaração de valores

---
+++ linguagens/golang/aula4/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var f float32
+	var b bool
+	var s string
+
+	//caso a variável seja criada e não seja declarado valor a mesmo a própria linguage atribui
+
+	fmt.Printf("inteiro: %v\n", i)
+	fmt.Printf("float: %v\n", f)
+	fmt.Printf("boolean: %v\n", b)
+	fmt.Printf("str: %v\n", s)
+}
