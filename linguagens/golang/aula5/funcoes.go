8eafd8b0bbd894aaefdcfd13903cd1d856186386
Laysa Siqueira <laysaasiqueiraa@gmail.com>
2024-06-27 11:51:58 +0100

    funções

--- linguagens/golang/aula5/funcoes.go
+++ linguagens/golang/aula5/funcoes.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println(soma(43, 14))
 
+	sub := subtracao(4, 6)
+	fmt.Println(sub)
+
 }
 
 func soma(x int, y int) int {
 	return x + y
+}
+
+func subtracao(x int, y int) int {
+	return x - y
 }
